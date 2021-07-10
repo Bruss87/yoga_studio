edc81cfe9ecf42dbe15ca9f517c8607ad5c18f8c
Karolina K <kaczmarska.kar@gmail.com>
2021-07-10 13:33:20 +0100

    adds first model schema for DB

---
+++ models/User.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+const { isEmail } = require('validator');
+
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: [true, 'Proszę podaj adres email'],
+        unique: true,
+        lowercase:true,
+        validate: [isEmail, 'Proszę podaj poprawny email']
+    },
+    password: {
+        type: String,
+        required: [true, 'Proszę podaj poprawne hasło'],
+        minlenght: [8, 'Minimalna długość hasła to 8 znaków']
+    },
+    register_date: {
+        type: Date,
+        default: Date.now
+    },
+})
+
+module.exports = User = mongoose.model('user', UserSchema);
